6a41a796d1c5b750c0a25dfa6f7dcfce5882f6ae
aliza amir <Alizaamir49@gmail.com>
2023-05-20 10:03:33 -0700

    preserved data

---
+++ Js/localstorage.js
@@ -0,0 +1,28 @@
+const formData = { name: "", email: "", message: "" };
+function populateStorage() {
+  formData.name = document.getElementById("name").value;
+  formData.email = document.getElementById("email").value;
+  formData.messege = document.getElementById("message").value;
+  localStorage.setItem("form-data", JSON.stringify(formData));
+}
+function setData() {
+  let storageData = localStorage.getItem("form-data");
+  storageData = JSON.parse(storageData);
+  document.getElementById("name").value = storageData.name;
+  document.getElementById("email").value = storageData.email;
+  document.getElementById("message").value = storageData.message;
+}
+const inputList = [];
+inputList.push(document.getElementById("name"));
+inputList.push(document.getElementById("email"));
+inputList.push(document.getElementById("message"));
+inputList.forEach((value) => {
+  value.addEventListener("input", () => {
+    populateStorage();
+  });
+});
+if (!localStorage.getItem("form-data")) {
+  populateStorage();
+} else {
+  setData();
+}

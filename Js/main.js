b3bd215a6d162bec45711be7eccaff5002ad9a8c
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-05-21 02:37:20 +0500

    Modify

--- Js/main.js
+++ Js/main.js
@@ -4,7 +4,9 @@ const submit = document.querySelectorAll('.see-project');
 const popshow = document.querySelector('.popup-container');
 const hidePopup = document.querySelector('.cross-popup');
 const hamburger = document.querySelector('#hamburger-button');
-
+const form = document.querySelector('.form');
+const errorMsg = document.querySelector('.flex');
+const email = document.querySelector('#email');
 function toggleMenue() {
   if (menue.classList.contains('hidden')) {
     menue.classList.remove('hidden');
@@ -14,7 +16,6 @@ function toggleMenue() {
     cross.classList.add('hidden');
   }
 }
-
 const togglePopup = () => {
   if (popshow.classList.contains('hidden')) {
     popshow.classList.remove('hidden');
@@ -22,19 +23,24 @@ const togglePopup = () => {
     popshow.classList.add('hidden');
   }
 };
-
 hamburger.addEventListener('click', toggleMenue);
 cross.addEventListener('click', toggleMenue);
-
 document.querySelectorAll('.menue-items').forEach((n) => {
   n.addEventListener('click', () => {
     menue.classList.add('hidden');
     cross.classList.add('hidden');
   });
 });
-
 submit.forEach((button) => {
   button.addEventListener('click', togglePopup);
 });
-
 hidePopup.addEventListener('click', togglePopup);
+form.addEventListener('submit', (e) => {
+  if (email.value.toLowerCase() !== email.value) {
+    e.preventDefault();
+    errorMsg.style.display = 'block';
+    errorMsg.innerHTML = 'Email must be in Lowercase';
+  } else {
+    errorMsg.style.display = 'none';
+  }
+});

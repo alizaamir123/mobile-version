117ec6dc8eb0f1e5e8f1575244ff5251e1b6df06
aliza amir <Alizaamir49@gmail.com>
2023-05-23 08:13:46 -0700

    add menu popup

--- Js/main.js
+++ Js/main.js
@@ -1,12 +1,6 @@
 const menue = document.querySelector(".menue-popup");
 const cross = document.querySelector("#cross-button");
-const submit = document.querySelectorAll(".see-project");
-const popshow = document.querySelector(".popup-container");
-const hidePopup = document.querySelector(".cross-popup");
 const hamburger = document.querySelector("#hamburger-button");
-const form = document.querySelector(".form");
-errorMsg = document.querySelector(".flex");
-email = document.querySelector("#email");
 function toggleMenue() {
   if (menue.classList.contains("hidden")) {
     menue.classList.remove("hidden");
@@ -16,13 +10,6 @@ function toggleMenue() {
     cross.classList.add("hidden");
   }
 }
-const togglePopup = () => {
-  if (popshow.classList.contains("hidden")) {
-    popshow.classList.remove("hidden");
-  } else {
-    popshow.classList.add("hidden");
-  }
-};
 hamburger.addEventListener("click", toggleMenue);
 cross.addEventListener("click", toggleMenue);
 document.querySelectorAll(".menue-items").forEach((n) => {
@@ -30,17 +17,4 @@ document.querySelectorAll(".menue-items").forEach((n) => {
     menue.classList.add("hidden");
     cross.classList.add("hidden");
   });
-});
-submit.forEach((button) => {
-  button.addEventListener("click", togglePopup);
-});
-hidePopup.addEventListener("click", togglePopup);
-form.addEventListener("submit", (e) => {
-  if (email.value.toLowerCase() !== email.value) {
-    e.preventDefault();
-    errorMsg.style.display = "block";
-    errorMsg.innerHTML = "Email must be in Lowercase";
-  } else {
-    errorMsg.style.display = "none";
-  }
 });

4b3eacb9f9697ca1deebb565f63e82e012a9f6f8
aliza amir <Alizaamir49@gmail.com>
2023-05-24 08:53:33 -0700

    windows popup

---
+++ Js/localstorage.js
@@ -0,0 +1,23 @@
+const formData = JSON.parse(localStorage.getItem("form-data")) || {
+  name: "",
+  email: "",
+  message: "",
+};
+
+const inputList = [
+  document.getElementById("name"),
+  document.getElementById("email"),
+  document.getElementById("message"),
+];
+
+function updateFormData() {
+  inputList.forEach((input) => {
+    input.addEventListener("input", () => {
+      formData[input.id] = input.value;
+      localStorage.setItem("form-data", JSON.stringify(formData));
+    });
+    input.value = formData[input.id];
+  });
+}
+
+updateFormData();
--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,12 @@
 const menue = document.querySelector('.menue-popup');
 const cross = document.querySelector('#cross-button');
 const hamburger = document.querySelector('#hamburger-button');
+const submit = document.querySelectorAll(".see-project");
+const popshow = document.querySelector(".popup-container");
+const hidePopup = document.querySelector(".cross-popup");
+const form = document.querySelector(".form");
+errorMsg = document.querySelector(".flex");
+email = document.querySelector("#email");
 const toggleMenue = () => {
   if (menue.classList.contains('hidden')) {
     menue.classList.remove('hidden');
@@ -10,7 +16,13 @@ const toggleMenue = () => {
     cross.classList.add('hidden');
   }
 };
-
+const togglePopup = () => {
+  if (popshow.classList.contains("hidden")) {
+    popshow.classList.remove("hidden");
+  } else {
+    popshow.classList.add("hidden");
+  }
+};
 hamburger.addEventListener('click', toggleMenue);
 cross.addEventListener('click', toggleMenue);
 document.querySelectorAll('.menue-items').forEach((n) => {
@@ -18,4 +30,17 @@ document.querySelectorAll('.menue-items').forEach((n) => {
     menue.classList.add('hidden');
     cross.classList.add('hidden');
   });
+});
+submit.forEach((button) => {
+  button.addEventListener("click", togglePopup);
+});
+hidePopup.addEventListener("click", togglePopup);
+form.addEventListener("submit", (e) => {
+  if (email.value.toLowerCase() !== email.value) {
+    e.preventDefault();
+    errorMsg.style.display = "block";
+    errorMsg.innerHTML = "Email must be in Lowercase";
+  } else {
+    errorMsg.style.display = "none";
+  }
 });

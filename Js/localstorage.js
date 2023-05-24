a1e77b6d2f79daf0d006658258be12faf9ed346e
aliza amir <Alizaamir49@gmail.com>
2023-05-24 09:20:01 -0700

    fixed

--- Js/localstorage.js
+++ Js/localstorage.js
@@ -1,20 +1,20 @@
-const formData = JSON.parse(localStorage.getItem("form-data")) || {
-  name: "",
-  email: "",
-  message: "",
+const formData = JSON.parse(localStorage.getItem('form-data')) || {
+  name: '',
+  email: '',
+  message: '',
 };
 
 const inputList = [
-  document.getElementById("name"),
-  document.getElementById("email"),
-  document.getElementById("message"),
+  document.getElementById('name'),
+  document.getElementById('email'),
+  document.getElementById('message'),
 ];
 
 function updateFormData() {
   inputList.forEach((input) => {
-    input.addEventListener("input", () => {
+    input.addEventListener('input', () => {
       formData[input.id] = input.value;
-      localStorage.setItem("form-data", JSON.stringify(formData));
+      localStorage.setItem('form-data', JSON.stringify(formData));
     });
     input.value = formData[input.id];
   });
